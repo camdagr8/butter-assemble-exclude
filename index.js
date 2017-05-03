91706b059d15efc09814ad630591705698490593
Cameron Tullos <ctullos@resourceammirati.com>
2017-05-03 11:12:27 -0400

    cleaned up code and added repo link to package.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+'use strict';
+
 const path = require('path');
 
 
 const exclude = function (filePath) {
 	let name = path.basename(filePath);
-	return name.match(/(^[0-9\.]+|)(__)/, "") ? true : false;
+	return !!name.match(/(^[0-9\.]+|)(__)/);
 };
 
 
 module.exports = function(config, params) {
 	let output = [];
-	params.files.forEach((file) => { if (exclude(file) !== true) { output.push(file); } });
+	params.files.forEach((file) => {
+	    if (!exclude(file)) { output.push(file); }
+	});
 	return output;
 };
